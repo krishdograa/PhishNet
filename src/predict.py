168a4377cbcf12dccc23a21fad70572c9674875e
krishdograa <krishdograa@gmail.com>
2025-03-30 16:34:12 +0530

    changes

--- src/predict.py
+++ src/predict.py
@@ -17,23 +17,7 @@ def predict_email(model_file, vectorizer_file, email_text):
     return "Phishing" if prediction[0] == 1 else "Not Phishing"
 
 if __name__ == "__main__":
-    email_text = """Hi Team,
-
-Hope you're having a productive week.
-
-Just a reminder about our Q3 planning meeting scheduled for next week. Please find the proposed agenda attached to this email.
-
-Meeting Details:
-
-Date: Wednesday, April 2nd, 2025
-Time: 9:00 AM - 11:00 AM PST
-Location: Conference Room B
-Please review the agenda beforehand and come prepared to discuss your team's progress and priorities for the next quarter.
-
-If you have any questions or cannot attend, please let me know by the end of the day on Monday.
-
-Thanks,
-John"""
+    email_text = """Urgen =t danger to your account secure it now."""
     print(email_text)
     result = predict_email("models/phishing_detector.pkl", "data/preprocessed_data.pkl", email_text)
     print(f"Prediction: {result}")
